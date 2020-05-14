a6d654f9a4f86724dc245f35e37ebace7a204e73
mauriciohike <mauriciorenanvs@gmail.com>
2020-05-14 16:05:05 -0300

    Updates

---
+++ precache-manifest.0cd5e8a673abab6ec1040ceaadd7bbb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d97701e30edfae293e843818d8e4a80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03b979b27c7f04ebf349",
+    "url": "/static/css/2.35fcc229.chunk.css"
+  },
+  {
+    "revision": "03b979b27c7f04ebf349",
+    "url": "/static/js/2.6674033e.chunk.js"
+  },
+  {
+    "revision": "782e4721c6f3123bc4a0599ab1cd1b7d",
+    "url": "/static/js/2.6674033e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14d1d7ac5002d1084b70",
+    "url": "/static/js/main.dd763d55.chunk.js"
+  },
+  {
+    "revision": "7383e21c62927adac175",
+    "url": "/static/js/runtime-main.73157ef4.js"
+  }
+]);
